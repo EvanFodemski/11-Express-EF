872592974878b8ef3d884abb186fae70c3038976
OFF-WHITE UNC <evanfd05@gmail.com>
2023-11-04 17:09:14 -0600

    bugs fixed in in index html and js, api routes almost done, but bugs to be fixed

--- Develop/Routes/apiroutes.js
+++ Develop/Routes/apiroutes.js
@@ -1,3 +1,35 @@
 const fs = require("fs");
 let data = JSON.parse(fs.readFileSync('./Devlop/db/db.json'))
+const util = require("util")
+const writeFileAsync = util.promisify(fs.writeFile);
 
+module.exports = function(app){
+app.get("/api/notes", function(req, res){
+
+    res.JSON(data);
+
+});
+app.get("/api/notes:id", function(req, res){
+    res.json(data[Number(req.params.id)]);
+
+});
+app.post("/api/notes", async function(req, res){
+    try{
+        let newNote = req.body;
+
+        let specialId = data.length.toString();
+
+        console.log(specialId);
+        newNote.id = specialId;
+        data.push(newNote);
+        await writeFileAsync("./db/db.json", JSON.stringify(data));
+        res.json(data)
+
+    } catch(error){
+        console.log(error);
+        res.status(500).json({error: "Server Error"})
+    }
+});
+
+
+}
